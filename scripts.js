90082dd8622971ddb0fe06c6dae9834f7b024659
qJulio <111520530+qJulio@users.noreply.github.com>
2024-10-25 18:27:11 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Añadir interactividad aquí más adelante
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Página cargada correctamente");
+    // Puedes añadir animaciones o eventos de clic aquí
+});
